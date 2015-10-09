e3f63c80faf3fc30523f4b649ea504ca3acdf284
Gerasimenko (G-Art) Artem <artem_herasymenko@epam.com>
2015-10-09 10:15:27 +0300

    improve test

--- src/test/java/com/epam/test/validator/InstructionMessageValidatorTest.java
+++ src/test/java/com/epam/test/validator/InstructionMessageValidatorTest.java
@@ -27,18 +27,67 @@ public class InstructionMessageValidatorTest {
 
     private SimpleDateFormat dateFormat;
 
+    private InstructionMessage correctMessage;
+    private InstructionMessage messageWithIncorrectInstructionType;
+    private InstructionMessage messageWithIncorrectProductCode;
+    private InstructionMessage messageWithIncorrectQuantity;
+    private InstructionMessage messageWithIncorrectUOM;
+    private InstructionMessage messageWithNullTimestamp;
+    private InstructionMessage messageWithIncorrectTimestamp;
+
+
     @Before
-    public void setUp(){
+    public void setUp() throws ParseException {
         dateFormat = new SimpleDateFormat(dateFormatPattern);
+
+        correctMessage = new InstructionMessage("A", "AZ19", 11, 11, dateFormat.parse("2015-03-05T10:04:51.012Z"));
+
+        messageWithIncorrectInstructionType = new InstructionMessage("", "AZ19", 1111, 11,
+                                                                     dateFormat.parse("2015-03-05T10:04:51.012Z"));
+        messageWithIncorrectProductCode = new InstructionMessage("A", "0000", 1111, 11,
+                                                                 dateFormat.parse("2015-03-05T10:04:51.012Z"));
+        messageWithIncorrectQuantity = new InstructionMessage("A", "AZ19", -1111, 11,
+                                                              dateFormat.parse("2015-03-05T10:04:51.012Z"));
+        messageWithIncorrectUOM = new InstructionMessage("A", "AZ19", 1111, -11,
+                                                         dateFormat.parse("2015-03-05T10:04:51.012Z"));
+        messageWithIncorrectTimestamp = new InstructionMessage("A", "AZ19", 1111, 11,
+                                                               dateFormat.parse("3000-03-05T10:04:51.012Z"));
+        messageWithNullTimestamp = new InstructionMessage("A", "AZ19", 1111, 11, null);
+
     }
 
     @Test
-    public void shouldBeValid() throws ParseException, ValidationException {
-        assertTrue(underTest.validate(new InstructionMessage("A", "MZ89", 5678, 50, dateFormat.parse("2015-03-05T10:04:51.012Z"))));
+    public void shouldAcceptWhenMessageIsCorrect() throws ValidationException {
+        assertTrue(underTest.validate(correctMessage));
     }
 
     @Test(expected = ValidationException.class)
-    public void shouldBeException() throws ParseException, ValidationException {
-        assertTrue(underTest.validate(new InstructionMessage("", "", 5678, 50, dateFormat.parse("2015-03-05T10:04:51.012Z"))));
+    public void shouldThrowExceptionWhenMessageWithIncorrectInstructionType() throws ValidationException {
+        assertTrue(underTest.validate(messageWithIncorrectInstructionType));
+    }
+
+    @Test(expected = ValidationException.class)
+    public void shouldThrowExceptionWhenMessageWithIncorrectProductCode() throws ValidationException {
+        assertTrue(underTest.validate(messageWithIncorrectProductCode));
+    }
+
+    @Test(expected = ValidationException.class)
+    public void shouldThrowExceptionWhenMessageWithIncorrectQuantity() throws ValidationException {
+        assertTrue(underTest.validate(messageWithIncorrectQuantity));
+    }
+
+    @Test(expected = ValidationException.class)
+    public void shouldThrowExceptionWhenMessageWithIncorrectUOM() throws ValidationException {
+        assertTrue(underTest.validate(messageWithIncorrectUOM));
+    }
+
+    @Test(expected = ValidationException.class)
+    public void shouldThrowExceptionWhenMessageWithNullTimestamp() throws ValidationException {
+        assertTrue(underTest.validate(messageWithNullTimestamp));
+    }
+
+    @Test(expected = ValidationException.class)
+    public void shouldThrowExceptionWhenMessageWithIncorrectTimestamp() throws ValidationException {
+        assertTrue(underTest.validate(messageWithIncorrectTimestamp));
     }
 }
